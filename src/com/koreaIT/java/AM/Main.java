6a084dda4e58b3dab5c9cfb37eb20457a7216918
woooyoung <axdsw121@gmail.com>
2022-02-18 12:30:04 +0900

    게시물 수정 기능 구현

--- src/com/koreaIT/java/AM/Main.java
+++ src/com/koreaIT/java/AM/Main.java
@@ -76,6 +76,37 @@ public class Main {
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 
+			} else if (command.startsWith("article modify ")) {
+
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]); // "1" -> 1
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				foundArticle.title = title;
+				foundArticle.body = body;
+				
+				System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+				
 			} else if (command.startsWith("article delete ")) {
 
 				String[] commandBits = command.split(" ");
