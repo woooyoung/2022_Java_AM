ff9836f8fc15eac293e7b94f969e2d6bfa8612c5
woooyoung <axdsw121@gmail.com>
2022-02-22 12:23:33 +0900

    회원가입 기능 구현(LoginId,Pw 중복체크)

--- src/com/koreaIT/java/AM/App.java
+++ src/com/koreaIT/java/AM/App.java
@@ -40,8 +40,20 @@ public class App {
 			if (command.equals("member join")) {
 				int id = members.size() + 1;
 				String regDate = Util.getNowDateTimeStr();
-				System.out.printf("로그인 아이디 : ");
-				String loginId = sc.nextLine();
+
+				String loginId;
+
+				while (true) {
+					System.out.printf("로그인 아이디 : ");
+					loginId = sc.nextLine();
+
+					if (isJoinableLoginId(loginId) == false) {
+						System.out.printf("%s(은)는 이미 사용중인 아이디입니다.\n", loginId);
+						continue;
+					}
+
+					break;
+				}
 
 				String loginPw = null;
 				String loginPwConfirm = null;
@@ -175,6 +187,29 @@ public class App {
 
 		sc.close();
 		System.out.println("== 프로그램 끝 ==");
+	}
+
+	private boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+
+		return -1;
 	}
 
 	private int getArticleIndexById(int id) {
